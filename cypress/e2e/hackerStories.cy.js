bacf385162141e17663cf2a787c08e7d812ae3b2
alissonKelvin <alisson.kelvindecarvalho@hotmail.com>
2023-06-13 17:41:28 -0300

    refactoring the test

--- cypress/e2e/hackerStories.cy.js
+++ cypress/e2e/hackerStories.cy.js
@@ -1,12 +1,78 @@
 import { faker } from '@faker-js/faker'
 
 describe('Hacker Stories', () => {
+ 
+  const initialTerm = 'React'
+  const newTerm = 'Cypress'
+
+  context('hitting the real API',() =>{
+    
+    beforeEach(() => {
+      cy.intercept({
+        method: 'GET',
+        pathname: '**/search',
+        query: {
+          query: initialTerm,
+          page: '0'
+        }
+      }).as('getStories')
+  
+      cy.visit('/')
+  
+      cy.wait('@getStories')
+    })
+
+    it('shows 20 stories, then the next 20 after clicking "More"', () => {
+      cy.intercept({
+        method: 'GET',
+        pathname: '**/search',
+        query: {
+          query: initialTerm,
+          page: '1'
+        }
+      }).as('getNextStories')
+
+      cy.get('.item').should('have.length', 20)
+
+      cy.contains('More').click()
+
+      cy.wait('@getNextStories')
+
+      cy.get('.item').should('have.length', 40)
+    })
+
+    it('searches via the last searched term', () => {
+
+      cy.intercept({
+        method: 'GET',
+        pathname: '**/search',
+        query: {
+          query: newTerm,
+          page: '0'
+        }
+      }).as('getNextStories')
+
+      cy.get('#search').clear().type(`${newTerm}{enter}`)
+
+      cy.wait('@getNextStories')
+
+      cy.get(`button:contains(${initialTerm})`).should('be.visible').click()
+
+      cy.wait('@getStories')
+
+      cy.get('.item').should('have.length', 20)
+      cy.get('.item').first().should('contain', initialTerm)
+      cy.get(`button:contains(${newTerm})`).should('be.visible')
+    })
+
+  })
+  
   beforeEach(() => {
     cy.intercept({
       method: 'GET',
       pathname: '**/search',
       query: {
-        query: 'React',
+        query: initialTerm,
         page: '0'
       }
     }).as('getStories')
@@ -16,6 +82,7 @@ describe('Hacker Stories', () => {
     cy.wait('@getStories')
   })
 
+  
   it('shows the footer', () => {
     cy.get('footer')
       .should('be.visible')
@@ -30,24 +97,7 @@ describe('Hacker Stories', () => {
     // TODO: Find a way to test it out.
     it.skip('shows the right data for all rendered stories', () => {})
 
-    it('shows 20 stories, then the next 20 after clicking "More"', () => {
-      cy.intercept({
-        method: 'GET',
-        pathname: '**/search',
-        query: {
-          query: 'React',
-          page: '1'
-        }
-      }).as('getNextStories')
-
-      cy.get('.item').should('have.length', 20)
-
-      cy.contains('More').click()
-
-      cy.wait('@getNextStories')
-
-      cy.get('.item').should('have.length', 40)
-    })
+    
 
     it('shows only nineteen stories after dimissing the first story', () => {
       cy.get('.button-small').first().click()
@@ -79,7 +129,7 @@ describe('Hacker Stories', () => {
           method: 'GET',
           pathname: '**/search',
           query: {
-            query: 'React',
+            query: initialTerm,
             page: '0'
           }
 
@@ -97,7 +147,7 @@ describe('Hacker Stories', () => {
           method: 'GET',
           pathname: '**/search',
           query: {
-            query: 'React',
+            query: initialTerm,
             page: '0'
           }
 
@@ -113,8 +163,7 @@ describe('Hacker Stories', () => {
   })
 
   context('Search', () => {
-    const initialTerm = 'React'
-    const newTerm = 'Cypress'
+   
 
     beforeEach(() => {
       cy.get('#search').clear()
@@ -151,20 +200,7 @@ describe('Hacker Stories', () => {
     })
 
     context('Last searches', () => {
-      it('searches via the last searched term', () => {
-        cy.get('#search').type(`${newTerm}{enter}`)
-
-        cy.wait('@getNextStories')
-
-        cy.get(`button:contains(${initialTerm})`).should('be.visible').click()
-
-        cy.wait('@getStories')
-
-        cy.get('.item').should('have.length', 20)
-        cy.get('.item').first().should('contain', initialTerm)
-        cy.get(`button:contains(${newTerm})`).should('be.visible')
-      })
-
+    
       it('shows a max of 5 buttons for the last searched terms', () => {
         Cypress._.times(6, () => {
           const names = faker.random.word()
